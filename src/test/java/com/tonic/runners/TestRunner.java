37b85232107170f1a73b18363465bbda6fff6c63
AbhinavSingh2202 <AbhinavS@vrize.com>
2025-06-04 09:10:09 -0400

    update parallel testng.xml
    update parallel testng.xml

--- src/test/java/com/tonic/runners/TestRunner.java
+++ src/test/java/com/tonic/runners/TestRunner.java
@@ -1,11 +1,10 @@
 package com.tonic.runners;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
     features = "src/test/resources/features/web",
     glue = {"com.tonic.stepDefinitions", "com.tonic.hooks"},
-    plugin = {"pretty", "summary"}, tags = "@RolesCopyModal"
+    plugin = {"pretty", "summary"}
 )
-public class TestRunner extends AbstractTestNGCucumberTests {}
+public class TestRunner extends TestNGBase {}
