3ff1fd9f40a0dc94c0a3675a21bb0415a21793ef
Prasvrize <Prasanna@vrize.com>
2025-06-04 18:15:41 +0530

    commit

--- src/test/java/com/tonic/stepDefinitions/web/CommonSteps.java
+++ src/test/java/com/tonic/stepDefinitions/web/CommonSteps.java
@@ -17,6 +17,7 @@ public class CommonSteps {
     ConfigurationPage configurationPage;
     TerminalsPage terminalsPage;
 
+    
     @Given("the user is logged in")
     public void the_user_is_logged_in() {
         PlaywrightFactory.getPage().navigate("https://admin.test.ordyx.com/login");
