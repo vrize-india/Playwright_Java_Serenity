df70ecefae8727c653866c6a884ef0c1968262ab
AbhinavSingh2202 <AbhinavS@vrize.com>
2025-06-04 10:59:47 -0400

    Cucumber Report Commit
    Cucumber Report Commit

--- src/test/java/com/tonic/runners/TestRunner.java
+++ src/test/java/com/tonic/runners/TestRunner.java
@@ -5,6 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features/web",
     glue = {"com.tonic.stepDefinitions", "com.tonic.hooks"},
-    plugin = {"pretty", "summary"}
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/cucumber.json"
+    }
 )
 public class TestRunner extends TestNGBase {}
