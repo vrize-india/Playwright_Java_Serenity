afe541e589d93d5b7c3cfed42b411fc7353b737a
AbhinavSingh2202 <AbhinavS@vrize.com>
2025-06-06 13:44:01 -0400

    Parametre based parallellisation
    Parametre based parallellisation

--- src/test/java/com/tonic/runners/TestRunner.java
+++ src/test/java/com/tonic/runners/TestRunner.java
@@ -1,13 +1,23 @@
 package com.tonic.runners;
 
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import io.qameta.allure.testng.AllureTestNg;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(
 
     features = "src/test/resources/features",
     glue = {"com.tonic.stepDefinitions", "com.tonic.hooks"},
     plugin = {"pretty", "summary", "html:target/cucumber-report.html", "json:target/cucumber-report.json"},
-    tags = "@inventory"
+        tags = "@TONIC11579"
 )
-public class TestRunner extends TestNGBase {}
+@Listeners({AllureTestNg.class})
+public class TestRunner  extends io.cucumber.testng.AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
 
